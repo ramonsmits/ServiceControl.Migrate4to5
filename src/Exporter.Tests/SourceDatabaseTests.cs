a7581a911a9b6b5b155495b9f9e93ece7ecc9418
Ramon Smits <ramon.smits@gmail.com>
2026-07-03 16:42:03 +0200

    ✨ RavenDB 3.5 source reader: prefix streaming, singletons, legacy attachments

---
+++ src/Exporter.Tests/SourceDatabaseTests.cs
@@ -0,0 +1,70 @@
+namespace ServiceControl.Migrate4to5.Exporter.Tests;
+
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Raven.Client.Embedded;
+using Raven.Json.Linq;
+using ServiceControl.Migrate4to5.DumpFormat;
+
+[TestFixture]
+public class SourceDatabaseTests
+{
+    EmbeddableDocumentStore store = null!;
+    SourceDatabase source = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        store = new EmbeddableDocumentStore { RunInMemory = true };
+        store.Conventions.SaveEnumsAsIntegers = true;
+        store.Initialize();
+        source = new SourceDatabase(store); // internal test ctor: SourceDatabase owns disposal
+
+        Put("FailedMessages/aaa", "FailedMessages", """{"UniqueMessageId":"aaa","Status":1}""");
+        Put("FailedMessages/bbb", "FailedMessages", """{"UniqueMessageId":"bbb","Status":4}""");
+        Put("CustomChecks/ccc", "CustomChecks", """{"CustomCheckId":"Check1","Status":0}""");
+        Put("RetryOperations/History", "RetryHistories", """{"HistoricOperations":[]}""");
+    }
+
+    void Put(string id, string entityName, string json) =>
+        store.DatabaseCommands.Put(id, null, RavenJObject.Parse(json),
+            new RavenJObject { { "Raven-Entity-Name", entityName } });
+
+    [TearDown]
+    public void TearDown() => source.Dispose();
+
+    [Test]
+    public void Streams_all_docs_with_prefix_and_metadata()
+    {
+        var docs = source.Stream(new CollectionSpec { Name = "FailedMessages", IdPrefix = "FailedMessages/" }).ToList();
+        Assert.That(docs.Select(d => d.Id), Is.EquivalentTo(new[] { "FailedMessages/aaa", "FailedMessages/bbb" }));
+        var aaa = docs.Single(d => d.Id == "FailedMessages/aaa");
+        Assert.That((string)aaa.Metadata["Raven-Entity-Name"]!, Is.EqualTo("FailedMessages"));
+        Assert.That((int)aaa.Document["Status"]!, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Fixed_id_spec_yields_one_doc_or_none()
+    {
+        Assert.That(source.Stream(new CollectionSpec { Name = "RetryHistory", FixedId = "RetryOperations/History" }).Count(), Is.EqualTo(1));
+        Assert.That(source.Stream(new CollectionSpec { Name = "MessageRedirects", FixedId = "messageredirects" }), Is.Empty);
+    }
+
+    [Test]
+    public void Reads_legacy_attachment_with_content_type()
+    {
+        var bytes = Encoding.UTF8.GetBytes("<big/>");
+#pragma warning disable 618
+        store.DatabaseCommands.PutAttachment("messagebodies/msg-9", null, new MemoryStream(bytes),
+            new RavenJObject { { "ContentType", "application/json" }, { "ContentLength", bytes.Length } });
+#pragma warning restore 618
+
+        var att = source.GetLegacyAttachment("msg-9");
+        Assert.That(att, Is.Not.Null);
+        Assert.That(att!.Value.Content, Is.EqualTo(bytes));
+        Assert.That(att.Value.ContentType, Is.EqualTo("application/json"));
+        Assert.That(source.GetLegacyAttachment("nope"), Is.Null);
+    }
+}
---
+++ src/Exporter/SourceDatabase.cs
@@ -0,0 +1,80 @@
+#nullable disable // Raven 3.5 API predates NRT
+namespace ServiceControl.Migrate4to5.Exporter;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Raven.Client.Embedded;
+using ServiceControl.Migrate4to5.DumpFormat;
+
+public sealed class SourceDatabase : IDisposable
+{
+    readonly EmbeddableDocumentStore store;
+
+    public SourceDatabase(string dbPath)
+        : this(new EmbeddableDocumentStore
+        {
+            DataDirectory = dbPath,
+            UseEmbeddedHttpServer = false,
+            EnlistInDistributedTransactions = false,
+        })
+    {
+        if (!Directory.Exists(dbPath))
+        {
+            throw new InvalidOperationException($"Database path not found: {dbPath}");
+        }
+    }
+
+    internal SourceDatabase(EmbeddableDocumentStore store)
+    {
+        this.store = store;
+        if (store.WasDisposed == false && store.DatabaseCommands == null)
+        {
+            store.Conventions.SaveEnumsAsIntegers = true;
+            store.Initialize();
+        }
+    }
+
+    public IEnumerable<(string Id, JObject Metadata, JObject Document)> Stream(CollectionSpec spec)
+    {
+        if (spec.FixedId != null)
+        {
+            var doc = store.DatabaseCommands.Get(spec.FixedId);
+            if (doc != null)
+            {
+                yield return (doc.Key, DumpJson.Parse(doc.Metadata.ToString()), DumpJson.Parse(doc.DataAsJson.ToString()));
+            }
+            yield break;
+        }
+
+        using var enumerator = store.DatabaseCommands.StreamDocs(startsWith: spec.IdPrefix, pageSize: int.MaxValue);
+        while (enumerator.MoveNext())
+        {
+            var raw = enumerator.Current;
+            var metadata = (Raven.Json.Linq.RavenJObject)raw["@metadata"];
+            var id = metadata.Value<string>("@id");
+            raw.Remove("@metadata");
+            yield return (id, DumpJson.Parse(metadata.ToString()), DumpJson.Parse(raw.ToString()));
+        }
+    }
+
+    public (byte[] Content, string ContentType)? GetLegacyAttachment(string bodyId)
+    {
+#pragma warning disable 618
+        var attachment = store.DatabaseCommands.GetAttachment("messagebodies/" + bodyId);
+#pragma warning restore 618
+        if (attachment == null)
+        {
+            return null;
+        }
+
+        using var stream = attachment.Data();
+        using var ms = new MemoryStream();
+        stream.CopyTo(ms);
+        var contentType = attachment.Metadata.Value<string>("ContentType") ?? "text/xml";
+        return (ms.ToArray(), contentType);
+    }
+
+    public void Dispose() => store.Dispose();
+}
